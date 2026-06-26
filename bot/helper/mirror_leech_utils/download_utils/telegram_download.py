f89a9578ae652cab172f13f789ec066133898080
Silent Demon SD ( MysterySD ) <105407900+SilentDemonSD@users.noreply.github.com>
2026-06-26 11:27:38 +0530

    fix: get_message for user/bot

--- bot/helper/mirror_leech_utils/download_utils/telegram_download.py
+++ bot/helper/mirror_leech_utils/download_utils/telegram_download.py
@@ -154,16 +154,22 @@ class TelegramDownloadHelper:
                 self._listener.transmission_mode in ("user", "both")
                 and self._listener.is_super_chat
             ):
-                self.session = "user"
-                try:
-                    message = await TgClient.user.get_messages(
-                        chat_id=message.chat.id, message_ids=message.id
-                    )
-                except (PeerIdInvalid, ChannelInvalid):
+                if not TgClient.user:
                     LOGGER.warning(
-                        "User session is not in this chat!, Downloading with bot session"
+                        "User session not available, downloading with bot session"
                     )
                     self.session = "bot"
+                else:
+                    self.session = "user"
+                    try:
+                        message = await TgClient.user.get_messages(
+                            chat_id=message.chat.id, message_ids=message.id
+                        )
+                    except (PeerIdInvalid, ChannelInvalid):
+                        LOGGER.warning(
+                            "User session is not in this chat, downloading with bot session"
+                        )
+                        self.session = "bot"
             else:
                 self.session = "bot"
         media = getattr(message, message.media.value) if message.media else None
@@ -207,7 +213,7 @@ class TelegramDownloadHelper:
                         message = await self._listener.client.get_messages(
                             chat_id=message.chat.id, message_ids=message.id
                         )
-                    else:
+                    elif TgClient.user:
                         try:
                             message = await TgClient.user.get_messages(
                                 chat_id=message.chat.id, message_ids=message.id
@@ -216,6 +222,10 @@ class TelegramDownloadHelper:
                             message = await self._listener.client.get_messages(
                                 chat_id=message.chat.id, message_ids=message.id
                             )
+                    else:
+                        message = await self._listener.client.get_messages(
+                            chat_id=message.chat.id, message_ids=message.id
+                        )
                     if self._listener.is_cancelled:
                         async with global_lock:
                             if self._id in GLOBAL_GID:
